1e222328b15669131e2339cd9b3c01796dca3efd
lu14012007 <108529912+lu14012007@users.noreply.github.com>
2024-04-05 11:27:55 -0300

    .cópia

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const botoes = document.querySelectorAll(".botao");
-const textos = document.querySelectorAll(".aba-conteudo");
+const textos = document.querySelectorAll(".aba-conteudo")
 
 for(let i=0;i < botoes.length;i++){
 botoes[i].onclick = function(){
@@ -15,7 +15,7 @@ textos[i].classList.add("ativo");
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date ("2024-10-05T00:00:00");
+const tempoObjetivo1 = new Date("2024-10-05T00:00:00");
 let tempoAtual = new Date();
 
-contadores[0] = .textContent = tempoObjetivo1 - tempoAtual;
+contadores[0].textContent = tempoObjetivo1 - tempoAtual;
